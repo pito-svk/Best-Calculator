a24419f04f55dd39ccbd80f92a6efeebbf008761
pito-svk <parad.peter@gmail.com>
2014-03-11 08:43:37 +0100

    refactored algorithm - solved for minus values

--- best_calculator.py
+++ best_calculator.py
@@ -1,13 +1,20 @@
 def best_calculator(numerator, denumerator, n=10):
-    result = ""
     assert n > 0
+    result = ""
+    minus = ""
+    if (numerator < 0  and denumerator > 0) or (numerator > 0 and denumerator < 0):
+        minus += "-"
+    if numerator == 0:
+        return 0
+    numerator = abs(numerator)
+    denumerator = abs(denumerator)
     while True:
         if numerator >= denumerator:
             value = numerator / denumerator
             result += str(value)
             numerator -= (value*denumerator)
             if numerator == 0:
-                return result
+                return  minus +result
             elif "." not in result:
                 result += "."
             increase = False
@@ -19,6 +26,5 @@ def best_calculator(numerator, denumerator, n=10):
             numerator *= 10
             increase = True
         if len(result[result.index("."):]) == n+1:
-            return result
+            return minus + result
 
-        
